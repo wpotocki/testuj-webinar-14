fe2963b846f72b5fd834bbc3e87d263a40d092c5
Wojtek Potocki <szkolenia@wojtekpotocki.pl>
2022-01-22 20:33:45 +0100

    fluent wait

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,6 +16,22 @@ public class WaitTest extends BaseTest {
         driver.findElement(By.tagName("button")).click();
 
         WebDriverWait waitForFinish = new WebDriverWait(driver, Duration.ofSeconds(10));
+        waitForFinish.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("finish")));
+
+
+        String finishText = driver.findElement(By.id("finish")).getText();
+        Assert.assertEquals(finishText, "Hello World!");
+    }
+
+    @Test
+    public void progressBarFluentWaitTest() {
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.tagName("button")).click();
+
+        FluentWait<WebDriver> waitForFinish = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofMillis(500));
+
         waitForFinish.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("finish")));
 
 
