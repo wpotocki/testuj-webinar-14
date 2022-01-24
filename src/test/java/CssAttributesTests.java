5a8f9c3fe1069f517e48ba002ca6a44a40d68617
Wojtek Potocki <szkolenia@wojtekpotocki.pl>
2022-01-24 19:12:07 +0100

    radio button

--- src/test/java/CssAttributesTests.java
+++ src/test/java/CssAttributesTests.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class CssAttributesTests extends BaseTest {
     private static WebElement modalAd;
---
+++ src/test/java/RadioButtonTests.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class RadioButtonTests  extends BaseTest {
+
+    @Test
+    public void thereAreThreeRadioButtonInFormTests() {
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+        List<WebElement> radioButtons = driver.findElements(By.name("gender"));
+
+        Assert.assertEquals(radioButtons.size(), 3);
+    }
+
+    @Test
+    public void selectFemaleAndAssertOthersAreNotSelectedTests() {
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement femaleRadio = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='radio'][value='female']")));
+
+        femaleRadio.click();
+        List<WebElement> selectedRadioButtons = driver.findElements(By.cssSelector("input[type=radio]:checked"));
+        Assert.assertEquals(selectedRadioButtons.size(), 1);
+    }
+}
